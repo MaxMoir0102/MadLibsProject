4e37e24ea06edb968dee7c75eae392bdde4c3b25
Kris Zebarth <kris.zebarth@ocsb.ca>
2026-09-14 00:03:04 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+/*******************************
+ * Name: Your name
+ * Class: ICS3U
+ * Date:Friday Sept 18
+ * Project Name: Mad Libs
+ * 
+ * Project Description
+ * You will read in multiple entries from the user and store the results in variables
+ * You will then insert those variables into the following story to create a MadLib style result
+ * 
+ * A recent survey informs us that one out of every PLURALNOUN1 owns a/an ADJECTIVE1 phone. 
+ * Fortunately, VERB_ING_1 over a mobile NOUN1 in recent years has improved ADVERB1. 
+ * Today, BODY_PART1-held PLURALNOUN2 are all the rage. 
+ * In restaurants, you find many PLURALNOUN3 talking ADVERB2 into their ADJECTIVE2 phones as they eat their NOUN2. 
+ * NUMBER1 percent of American PLURALNOUN4 place their NOUN3 calls from their cars as they are VERB_ING_2 to and from their home, office, or NOUN4. 
+ * Walking and talking are now the “in” NOUN5 to do. 
+ * Over NUMBER2 percent of Americans walk our ADJECTIVE3 streets with a handheld PLURANOUN5 pressed against their BODYPART2.
+ * 
+ * Ask for the capitalized words in the input. Print out the full story in the output.
+ ********************************/
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// YOUR CODE GOES HERE!!!!
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		in.close();//Don't forget to clean up after yourselves!
+	}
+
+}
